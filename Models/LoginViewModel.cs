c06044996c690601ff4786da94fc7d1676f35407
Iamvalentina2020 <valentinavalda446@gmail.com>
2025-09-16 20:32:51 -0400

    login

---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace GestionPedidos.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "El email es requerido")]
+        [EmailAddress(ErrorMessage = "Formato de email inválido")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La contraseña es requerida")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        public bool RememberMe { get; set; }
+        public string? ReturnUrl { get; set; }
+    }
+}
