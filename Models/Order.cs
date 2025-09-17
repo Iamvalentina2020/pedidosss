83196016a40fe2ad0dc9271736919930605cea7f
Iamvalentina2020 <valentinavalda446@gmail.com>
2025-09-16 20:07:05 -0400

    order

---
+++ Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace pedidosss.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string ClienteNombre { get; set; }
+
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        public string Estado { get; set; } = "Pendiente";
+
+        public decimal Total { get; set; }
+
+        public List<OrderItem> Items { get; set; } = new();
+    }
+}
