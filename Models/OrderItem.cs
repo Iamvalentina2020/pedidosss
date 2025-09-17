8a3b51d99db7fb31de094e48af3867bb8dbbdd88
Iamvalentina2020 <valentinavalda446@gmail.com>
2025-09-16 20:29:48 -0400

    orderitemact

--- Models/OrderItem.cs
+++ Models/OrderItem.cs
@@ -1,19 +1,33 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace pedidosss.Models
+namespace GestionPedidos.Models.Domain
 {
     public class OrderItem
     {
         public int Id { get; set; }
 
         [Required]
-        public int ProductoId { get; set; }
-        public Product Producto { get; set; }
+        public int OrderId { get; set; }
 
         [Required]
-        public int Cantidad { get; set; }
+        public int ProductId { get; set; }
 
         [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser mayor a 0")]
+        public int Quantity { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal UnitPrice { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Subtotal { get; set; }
+
+        [ForeignKey("OrderId")]
+        public virtual Order Order { get; set; } = null!;
+
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; } = null!;
     }
 }
