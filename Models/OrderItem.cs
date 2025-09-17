1a19d70893f08db4fe8b4ef8d8052daf6662023c
Iamvalentina2020 <valentinavalda446@gmail.com>
2025-09-16 20:08:01 -0400

    orderitem

---
+++ Models/OrderItem.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pedidosss.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int ProductoId { get; set; }
+        public Product Producto { get; set; }
+
+        [Required]
+        public int Cantidad { get; set; }
+
+        [Required]
+        public decimal Subtotal { get; set; }
+    }
+}
